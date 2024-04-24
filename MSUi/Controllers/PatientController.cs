f1a7a7d429180f83d1c4eb2a3b565756532d4eca
Philippe CHARLES <A114SR@login.axa>
2024-04-24 17:25:09 +0200

    Mise en place de MSUi

---
+++ MSUi/Controllers/PatientController.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MSUi.Models;
+using Newtonsoft.Json;
+
+namespace MSUI.Controllers
+{
+    public class PatientController : Controller
+    {
+
+        private readonly HttpClient _httpClient;
+
+
+
+        public PatientController(HttpClient httpClient, IHttpClientFactory clientFactory)
+        {
+
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://Localhost:6001");
+
+        }
+
+        //    public async Task<List<Patient>> GetPatients()
+
+        //    {
+        //        using HttpResponseMessage response = await _httpClient.GetAsync("/api/Patient");
+        //        response.EnsureSuccessStatusCode();
+        //        using var responseStream = await response.Content.ReadAsStreamAsync();
+        //        return await JsonSerializer.DeserializeAsync<List<Patient>>(responseStream);
+
+        //    }
+
+        //    public async Task<IActionResult> Index()
+        //    {
+        //        var patients = await GetPatients();
+        //        return View(patients);
+        //    }
+        //}
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("/api/patient"); // Utiliser un endpoint spécifique pour récupérer tous les patients
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                var patients = JsonConvert.DeserializeObject<List<Patient>>(responseData);
+                return View(patients);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, $"Erreur HTTP: {response.StatusCode}");
+            }
+        }
+    }
+}
+
---
+++ MSUi/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSUi.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "le prénom est requis")]
+        public string Prenom { get; set; }
+        [Required(ErrorMessage = "le nom est requis")]
+        public string Nom { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Veuillez entrer une date de naissance valide.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateDeNaissance { get; set; }
+        [Required(ErrorMessage = "Le genre est requis")]
+        public string Genre { get; set; }
+        public string Adresse { get; set; }
+        public string Tel { get; set; }
+    }
+}
